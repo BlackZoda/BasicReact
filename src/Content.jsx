7f6e7075d729c9a5552877bd418f606978d6d1e5
Ivar Abusdal <ivar.abusdal@designerkontoret.no>
2024-01-03 23:52:52 +0100

    useState gotchas

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,16 +1,39 @@
+import { useState } from 'react';
+
 const Content = () => {
+    const [ name, setName ] = useState('Ivar');
+    const [ count, setCount ] = useState(0);
 
     const handleNameChange = () => {
         const names = ["Ivar", "Eivind", "Gunnar", "Siri"];
         const rndInt = Math.floor(Math.random() * names.length);
-        return names[rndInt];
+        setName(names[rndInt]);
+    }
+
+    const handleCount = () => {
+        setCount(count + 1);
+        setCount(count + 1);
+        console.log(count);
+    }
+
+    const getCount = () => {
+        console.log(count);
     }
 
     return (
         <main>
             <p>
-                Hello {handleNameChange()}!
+                Hello {name}!
             </p>
+            <button onClick={handleNameChange} type="button">
+                Change name
+            </button>
+            <button onClick={handleCount} type="button">
+                Increase Count
+            </button>
+            <button onClick={getCount} type="button">
+                Get Count
+            </button>
         </main>
     )
 }
